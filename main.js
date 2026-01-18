6d95cba4db0e1633e623d278bb27a758c390eafb
JeanPCB <92955454+JeanPCB@users.noreply.github.com>
2026-01-18 19:28:39 -0300

    Create main function -> get computer choice

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
+function getComputerChoice() {
+   let randNum = generateRandNum(1, 9);
+   if (randNum >= 1 && randNum <= 3) {
+      return "rock";
+   } else if (randNum >= 4 && randNum <= 6) {
+      return "paper";
+   } else if (randNum >= 7 && randNum <= 9) {
+      return "scissors";
+   } else {
+      return "Invalid input!";
+   }
+}
+
 function generateRandNum(includedMin, includedMax) {
    return Math.floor(Math.random() * (includedMax - includedMin + 1)) + 1;
 }
