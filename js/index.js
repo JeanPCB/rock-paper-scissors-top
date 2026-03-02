ef2695c442d97d82044a687bd33fb9119bd30343
JeanPCB <92955454+JeanPCB@users.noreply.github.com>
2026-03-01 21:22:42 -0300

    Remove main execution wrapper

--- js/index.js
+++ js/index.js
@@ -3,109 +3,107 @@ import { playsBtns, setPlayDisplay } from "./uiEvents.js";
 const playerDisplayContent = document.querySelector("#player-play-display");
 const computerDisplayContent = document.querySelector("#computer-play-display");
 
-function playGame() {
-  let humanScore = 0;
-  let computerScore = 0;
+let humanScore = 0;
+let computerScore = 0;
 
-  playsBtns.forEach((playBtn) =>
-    playBtn.addEventListener("click", () => {
-      const humanPlay = getHumanChoice(playBtn);
-      const computerPlay = getComputerChoice();
+playsBtns.forEach((playBtn) =>
+  playBtn.addEventListener("click", () => {
+    const humanPlay = getHumanChoice(playBtn);
+    const computerPlay = getComputerChoice();
 
-      playRound(computerPlay, humanPlay);
-      setPlayDisplay(humanPlay, playerDisplayContent);
-      setPlayDisplay(computerPlay, computerDisplayContent);
-    }),
-  );
+    playRound(computerPlay, humanPlay);
+    setPlayDisplay(humanPlay, playerDisplayContent);
+    setPlayDisplay(computerPlay, computerDisplayContent);
+  }),
+);
 
-  function playRound(computerChoice, humanChoice) {
-    switch (computerChoice) {
-      case "rock":
-        if (humanChoice === "paper") {
-          humanScore++;
-          printResult(computerChoice, humanChoice, "win");
-        } else if (humanChoice === "scissors") {
-          computerScore++;
-          printResult(computerChoice, humanChoice, "lose");
-        } else {
-          printResult(computerChoice, humanChoice, "draw");
-        }
-        break;
+function playRound(computerChoice, humanChoice) {
+  switch (computerChoice) {
+    case "rock":
+      if (humanChoice === "paper") {
+        humanScore++;
+        printResult(computerChoice, humanChoice, "win");
+      } else if (humanChoice === "scissors") {
+        computerScore++;
+        printResult(computerChoice, humanChoice, "lose");
+      } else {
+        printResult(computerChoice, humanChoice, "draw");
+      }
+      break;
 
-      case "paper":
-        if (humanChoice === "scissors") {
-          humanScore++;
-          printResult(computerChoice, humanChoice, "win");
-        } else if (humanChoice === "rock") {
-          computerScore++;
-          printResult(computerChoice, humanChoice, "lose");
-        } else {
-          printResult(computerChoice, humanChoice, "draw");
-        }
-        break;
+    case "paper":
+      if (humanChoice === "scissors") {
+        humanScore++;
+        printResult(computerChoice, humanChoice, "win");
+      } else if (humanChoice === "rock") {
+        computerScore++;
+        printResult(computerChoice, humanChoice, "lose");
+      } else {
+        printResult(computerChoice, humanChoice, "draw");
+      }
+      break;
 
-      case "scissors":
-        if (humanChoice === "rock") {
-          humanScore++;
-          printResult(computerChoice, humanChoice, "win");
-        } else if (humanChoice === "paper") {
-          computerScore++;
-          printResult(computerChoice, humanChoice, "lose");
-        } else {
-          printResult(computerChoice, humanChoice, "draw");
-        }
-        break;
+    case "scissors":
+      if (humanChoice === "rock") {
+        humanScore++;
+        printResult(computerChoice, humanChoice, "win");
+      } else if (humanChoice === "paper") {
+        computerScore++;
+        printResult(computerChoice, humanChoice, "lose");
+      } else {
+        printResult(computerChoice, humanChoice, "draw");
+      }
+      break;
 
-      default:
-        return null;
-    }
+    default:
+      return null;
+  }
+}
+
+function printResult(computerChoice, humanChoice, result) {
+  let resultPhrase = "That's a draw...";
+
+  if (result === "win") {
+    resultPhrase = `You ${result}! ${
+      humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)
+    } beats ${
+      computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)
+    }`;
+  } else if (result === "lose") {
+    resultPhrase = `You ${result}! ${
+      computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)
+    } beats ${humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)}`;
   }
 
-  function printResult(computerChoice, humanChoice, result) {
-    let resultPhrase = "That's a draw...";
+  console.log(resultPhrase);
+  printScore();
+}
 
-    if (result === "win") {
-      resultPhrase = `You ${result}! ${
-        humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)
-      } beats ${
-        computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)
-      }`;
-    } else if (result === "lose") {
-      resultPhrase = `You ${result}! ${
-        computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)
-      } beats ${humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)}`;
-    }
+function printScore() {
+  console.log("Human Score: " + humanScore);
+  console.log("Computer Score: " + computerScore);
+}
 
-    console.log(resultPhrase);
-    printScore();
+function getComputerChoice() {
+  let randNum = generateRandNum(1, 9);
+  if (randNum >= 1 && randNum <= 3) {
+    return "rock";
+  } else if (randNum >= 4 && randNum <= 6) {
+    return "paper";
+  } else if (randNum >= 7 && randNum <= 9) {
+    return "scissors";
+  } else {
+    return "Invalid input!";
   }
+}
 
-  function printScore() {
-    console.log("Human Score: " + humanScore);
-    console.log("Computer Score: " + computerScore);
-  }
+function getHumanChoice(button) {
+  const choice = button.textContent.toLowerCase();
+  return choice;
+}
 
-  function getComputerChoice() {
-    let randNum = generateRandNum(1, 9);
-    if (randNum >= 1 && randNum <= 3) {
-      return "rock";
-    } else if (randNum >= 4 && randNum <= 6) {
-      return "paper";
-    } else if (randNum >= 7 && randNum <= 9) {
-      return "scissors";
-    } else {
-      return "Invalid input!";
-    }
-  }
-
-  function getHumanChoice(button) {
-    const choice = button.textContent.toLowerCase();
-    return choice;
-  }
-
-  function generateRandNum(includedMin, includedMax) {
-    return Math.floor(Math.random() * (includedMax - includedMin + 1)) + 1;
-  }
+function generateRandNum(includedMin, includedMax) {
+  return Math.floor(Math.random() * (includedMax - includedMin + 1)) + 1;
 }
 
 playGame();
